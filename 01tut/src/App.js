90094d033c2627f8b43b6b7e47745977c884b9db
CrimsonCoder42 <nostro37@gmail.com>
2023-06-14 13:45:56 -0400

    03:29:01

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Header from './Header';
 import SearchItem from './SearchItem';
@@ -8,33 +8,48 @@ import Footer from './Footer';
 import AddItem from './AddItem';
 
 function App() {
-   const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
+  const API_URL = 'http://localhost:3500/items';
+
+   const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState('');
   const [search, setSearch] = useState('');
 
-  const setAndSaveItems = (listItems) => {
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-  }
+  useEffect(() => {
+    
+    const fetchItems = async () => {
+      try {
+        const res = await fetch(API_URL);
+        const data = await res.json();
+        console.log(data);
+        setItems(data);
+
+      } catch (error) {
+        console.log(error.stack);
+      }
+      
+    }
+   fetchItems();
+  }, [])
+
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
     
   }
 
 
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-    setAndSaveItems(listItems);
+    setItems(listItems);
     
   }
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
     
   }
 
