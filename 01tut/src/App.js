e1632a0ce91c1ff0ebc26f38ee69dd6467ef3673
CrimsonCoder42 <nostro37@gmail.com>
2023-06-14 06:43:47 -0400

    02:26:52

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -4,41 +4,57 @@ import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
 
-function App() {
-   const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half pound bag of Cocoa Covered Almonds Unsalted"
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2"
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3"
-    }
-  ]);
+import AddItem from './AddItem';
 
-  const handleCheck = (id) => {
-    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+function App() {
+   const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
+  const [newItem, setNewItem] = useState('');
+
+  const setAndSaveItems = (listItems) => {
     setItems(listItems);
     localStorage.setItem('shoppinglist', JSON.stringify(listItems));
   }
 
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+    
+  }
+
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setAndSaveItems(listItems);
+    
+  }
+
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    setAndSaveItems(listItems);
+    
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    // add item to list 
+    addItem(newItem);
+    // clear input
+    setNewItem('');
+    console.log('handleSubmit');
   }
 
 
   return (
     <div className="App">
       <Header title='Groceries'/>
+      <AddItem 
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
       <Content 
         items={items}
         handleCheck={handleCheck}
--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -1,10 +1,6 @@
-import { FaTrashAlt } from 'react-icons/fa';
 import ItemList from './ItemList';
 
-
 const Content = ({ items, handleCheck, handleDelete }) => {
-
-
   return (
     <main>
       {items.length ? (
@@ -13,12 +9,10 @@ const Content = ({ items, handleCheck, handleDelete }) => {
           handleCheck={handleCheck}
           handleDelete={handleDelete}
         />
-      ) :
-        (
-          <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
-        )}
+      ) : (
+        <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+      )}
     </main>
-
   )
 }
 
